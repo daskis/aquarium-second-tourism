29429b716310b81b9e96f4ce4ab67990c59a22e1
Artem Darius Weber <mit.3tlasa@gmail.com>
2024-04-21 05:08:13 +0300

    devops

--- sc-messenger/libs/api/chat/CONFIG/AGWServicePath.config.ts
+++ sc-messenger/libs/api/chat/CONFIG/AGWServicePath.config.ts
@@ -3,8 +3,8 @@
 //   import.meta.env.AGW_SERVICE_PORT || 3099 +
 //   '/api/chat/';
 
-export const MESSENGER_AGW_URL = "https://e.k-lab.su" +
+export const MESSENGER_AGW_URL = "https://f.k-lab.su/api" +
   '/api/chat/';
 
-export const MESSAGE_AGW_URL = "https://e.k-lab.su" +
+export const MESSAGE_AGW_URL = "https://f.k-lab.su/api" +
   '/api/message/';
